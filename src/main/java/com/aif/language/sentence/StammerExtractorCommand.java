0ba8c1e8c93de4c6465f9dca197fcac5f62867f8
Alex Balenko <podorozhnick@gmail.com>
2014-10-09 09:06:28 +0300

    Add StammerExtractorCommand

--- src/main/java/com/aif/language/sentence/StammerExtractorCommand.java
+++ src/main/java/com/aif/language/sentence/StammerExtractorCommand.java
@@ -1,7 +1,46 @@
 package com.aif.language.sentence;
 
-/**
- * Created by Aleksey on 09.10.14.
- */
-public class StammerExtractorCommand {
+import com.aif.cli.common.FileHelper;
+import com.aif.language.common.ISplitter;
+import com.aif.language.sentence.splitters.AbstractSentenceSplitter;
+import com.aif.language.token.TokenSplitter;
+import com.aif.language.word.AbstractWord;
+import com.aif.language.word.IWordExtractor;
+import com.aif.language.word.WordExtractor;
+
+import java.io.IOException;
+import java.util.List;
+
+public class StammerExtractorCommand extends BasicTextCommand {
+
+    private static final String STAMMER_TEMPLATE = "Stammer: [ %s ]";
+
+    private void printListOfStammers(List<AbstractWord.WordPlaceHolder> listOfStammers) {
+        for(AbstractWord.WordPlaceHolder stammer: listOfStammers) {
+            printStammer(stammer);
+        }
+    }
+
+    private void printStammer(AbstractWord.WordPlaceHolder stammer) {
+
+        System.out.println(String.format(STAMMER_TEMPLATE, stammer));
+
+    }
+
+    @Override
+    public Void apply(String... args) {
+        final String text;
+        try {
+            text = FileHelper.readAllTextFromFile(args[0]);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+        final TokenSplitter tokenSplitter = new TokenSplitter();
+        final ISplitter<List<String>, List<String>> sentenceSplitter = AbstractSentenceSplitter.Type.HEURISTIC.getInstance();
+        final IWordExtractor wordExtractor = new WordExtractor();
+        final List<List<AbstractWord.WordPlaceHolder>> result = wordExtractor.getWords(sentenceSplitter.split(tokenSplitter.split(text)));
+        result.forEach(this::printListOfStammers);
+        return null;
+    }
 }
