284cdea231a8877527ea5a5fedaf3d72a20df057
Artyom-Ganev <a.a.ganev@gmail.com>
2016-10-27 17:12:55 +0300

    ResultPrinter refactoring

--- src/main/java/com/aif/cli/common/ResultPrinter.java
+++ src/main/java/com/aif/cli/common/ResultPrinter.java
@@ -3,10 +3,10 @@ package com.aif.cli.common;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import io.aif.associations.builder.AssociationGraph;
 import io.aif.language.common.IDict;
@@ -23,87 +23,45 @@ public class ResultPrinter {
   public static void printSentenceSplitResult(List<List<String>> result) {
     final String template = "Sentence: [ %s ]";
     length = result.size();
-    result.forEach(s -> {
-      if (!stop) {
-        System.out.println(String.format(template, sentenceToString(s)));
-        length--;
-        count++;
-        stopper();
-      }
-    });
-
+    result.forEach(s -> printIfNotStop(String.format(template, sentenceToString(s))));
   }
 
   public static void printSentenceSeparatorSplitResult(Map<ISeparatorGroupsClassifier.Group, Set<Character>> result) {
     final String template = "Group: \'%s\', characters: %s\n";
     length = result.size();
-    result.keySet().forEach(s -> {
-      if (!stop) {
-        System.out.println(String.format(template, s, charactersToString(result.get(s))));
-        length--;
-        count++;
-        stopper();
-      }
-    });
+    result.keySet().forEach(s -> printIfNotStop(String.format(template, s, charactersToString(result.get(s)))));
   }
 
   public static void printTokeSplitResult(List<String> result) {
     final String template = "Token: [ %s ]";
     length = result.size();
-    result.forEach(token -> {
-      if (!stop) {
-        System.out.println(String.format(template, token));
-        length--;
-        count++;
-        stopper();
-      }
-    });
+    result.forEach(token -> printIfNotStop(String.format(template, token)));
   }
 
   public static void printSeparatorExtractResult(List<Character> result) {
     final String template = "Separator: \\u%s";
     length = result.size();
-    result.forEach(separator -> {
-      if (!stop) {
-        System.out.println(String.format(template, Integer.toHexString(separator)));
-        length--;
-        count++;
-        stopper();
-      }
-    });
+    result.forEach(separator -> printIfNotStop(String.format(template, Integer.toHexString(separator))));
   }
 
   public static void printStammerExtrctResult(IDict<IWord> result) {
     final String template = "Basic token: %-20s count: [ %-10d ] tokens: [ %s ]";
 
     length = result.getWords().size();
-    List<IWord> words = new ArrayList<>(result.getWords());
-    words.sort((w1, w2) -> w2.getCount().compareTo(w1.getCount()));
-    words.forEach(word -> {
-      if (!stop) {
-        System.out.println(String.format(template, word.getRootToken(), word.getCount(), word.getAllTokens()));
-        length--;
-        count++;
-        stopper();
-      }
-    });
+    result.getWords()
+      .stream()
+      .sorted((w1, w2) -> w2.getCount().compareTo(w1.getCount()))
+      .forEach(word -> printIfNotStop(String.format(template, word.getRootToken(), word.getCount(), word.getAllTokens())));
   }
 
   public static void printSemanticDictionaryBuildResult(AssociationGraph<IWord> result) {
     final String template = "Semantic node: %-20s, with weight = %.5f.";
 
     length = result.getVertices().size();
-    List<IWord> words = new ArrayList<>(result.getVertices());
-    words.sort((w1, w2) -> result.getVertexWeight(w2).compareTo(result.getVertexWeight(w1)));
-    words.forEach(word -> {
-      if (!stop) {
-        System.out.println(String.format(template, word.getRootToken(), result.getVertexWeight(word)));
-        length--;
-        count++;
-        stopper();
-      }
-    });
-
+    result.getVertices()
+      .stream()
+      .sorted((w1, w2) -> result.getVertexWeight(w2).compareTo(result.getVertexWeight(w1)))
+      .forEach(word -> printIfNotStop(String.format(template, word.getRootToken(), result.getVertexWeight(word))));
     FileHelper.saveSemanticResult(result);
   }
 
@@ -123,10 +81,10 @@ public class ResultPrinter {
           e.printStackTrace();
         }
 
-        if (command.equals("")) {
+        if ("".equals(command)) {
           count = 0;
           clicked = true;
-        } else if (command.equals("q")) {
+        } else if ("q".equals(command)) {
           stop = true;
           return;
         }
@@ -135,23 +93,20 @@ public class ResultPrinter {
   }
 
   private static String sentenceToString(List<String> sentence) {
-    final StringBuilder sentenceBuilder = new StringBuilder();
-    for (String token : sentence) {
-      if (sentenceBuilder.length() > 1)
-        sentenceBuilder.append(" ");
-      sentenceBuilder.append(token);
-    }
-
-    return sentenceBuilder.toString();
+    return sentence.stream().collect(Collectors.joining(" "));
   }
 
   private static String charactersToString(final Set<Character> characters) {
-    final StringBuilder stringBuilder = new StringBuilder();
-    stringBuilder.append('[');
-    characters.forEach(ch -> stringBuilder.append(ch + " "));
-    stringBuilder.deleteCharAt(stringBuilder.length() - 1);
-    stringBuilder.append(']');
-    return stringBuilder.toString();
+    return '[' + characters.stream().map(String::valueOf).collect(Collectors.joining(" ")) + ']';
+  }
+
+  private static void printIfNotStop(String s) {
+    if (!stop) {
+      System.out.println(s);
+      length--;
+      count++;
+      stopper();
+    }
   }
 
 }
